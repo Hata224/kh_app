ea979c5d4cbc45bfa3122cf2074aeed9bd761c05
Hata <kh6305su@gmail.com>
2019-11-24 17:24:17 +0900

    [update]titleタグの表記を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def full_title(page_title = '')
+    main_title = 'クラコピ'
+    if page_title.empty?
+      mail_title
+    else
+      page_title + ' | ' + main_title
+    end
+  end
 end
