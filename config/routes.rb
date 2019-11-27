19b9b16b2fae668377a374d7dfd48c5c7a2da74d
Hata <kh6305su@gmail.com>
2019-11-27 15:21:35 +0900

    [update]フォーム画面のデザイン変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@
 
 Rails.application.routes.draw do
   devise_for :users
-
-  root to: 'pages#home'
-  get 'pages/show'
+  devise_scope :user do
+    patch 'user' => 'devise_invitable/registrations#update', as: nil
+    put 'user' => 'devise_invitable/registrations#update',
+        as: :update_user_registration
+    delete 'user' => 'devise_invitable/registrations#destroy',
+           as: :destroy_user_registration
+    patch 'password' => 'devise/passwords#update'
+    put 'password' => 'devise/passwords#update', as: :update_user_password
+    resources :users
+    root to: 'pages#home'
+    get 'pages/show'
+  end
 end
