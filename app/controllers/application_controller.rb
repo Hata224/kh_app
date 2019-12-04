ee1978f91f5f9bee96ed1cdfd9f9b14d1e9c94f0
Hata <kh6305su@gmail.com>
2019-12-04 20:50:29 +0900

    [update]各フォーム画面にアイコンを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
     pages_show_path
-    end
+  end
 
   private
 
