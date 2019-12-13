3098da4fe3112034111bba1d33547be6ae80d49e
Hata <kh6305su@gmail.com>
2019-12-13 18:57:04 +0900

    [update]投稿編集ページのレイアウトを変更

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 
-  get 'pages/:id', to: 'pages#show'
+  get 'pages/show'
   get 'posts/index', to: 'posts#index'
   resources :posts
   resources :users, only: [:show]
