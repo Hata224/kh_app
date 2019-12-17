f1cc69d3a39e59bb3ea8d5634c5e61023f25325e
Hata <kh6305su@gmail.com>
2019-12-17 16:24:13 +0900

    [add]ユーザー編集ページに画像アップロード機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_040209) do
+ActiveRecord::Schema.define(version: 2019_12_17_065600) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2019_12_17_040209) do
     t.string "uid"
     t.string "username"
     t.text "profile"
-    t.string "avatar"
+    t.string "avatar_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
