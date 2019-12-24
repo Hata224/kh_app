f0e403da07e7ee427f104c43e466d8fc30a81ee9
Hata <kh6305su@gmail.com>
2019-12-24 13:55:57 +0900

    [update]ユーザー名の文字数の上限を５０文字以内に設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable
 
-  validates :username, presence: true
+  validates :username, presence: true, length: { maximum: 50 }
   validates :profile, length: { maximum: 500 }
 
   has_many :posts, dependent: :destroy
