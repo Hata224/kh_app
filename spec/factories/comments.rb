f819328d3f8e4443d6be9773b2a0d0135c0d287f
Hata <kh6305su@gmail.com>
2019-12-25 22:11:21 +0900

    [update]バリデーションに関するテストを追記

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :comment do
     content { 'MyString' }
-    user { nil }
-    post { nil }
+    association :post
+    association :user
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,7 +11,27 @@ RSpec.describe Post, type: :model do
     )
   end
 
-  it 'ファクトリが有効であること' do
+  it 'タイトルと本文が有効であること' do
     expect(@post).to be_valid
+  end
+
+  describe '投稿のバリデーションについて' do
+    it 'タイトルがなければ無効であること' do
+      @post.title = nil
+      @post.valid?
+      expect(@post.errors[:title]).to include('を入力してください')
+    end
+
+    it '本文がなければ無効であること' do
+      @post.body = nil
+      @post.valid?
+      expect(@post.errors[:body]).to include('を入力してください')
+    end
+
+    it '投稿にuser_idがなければ無効であること' do
+      @post = Post.create(user_id: nil)
+      @post.valid?
+      expect(@post.errors[:title]).to include 'を入力してください'
+    end
   end
 end
