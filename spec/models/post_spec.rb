b0d8e14ec9d9c4de2e0d0dd07766350ced2b00c3
Hata <kh6305su@gmail.com>
2019-12-25 21:00:19 +0900

    [update]バリデーションに関するテストを追記

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @post = @user.posts.create(
+      title: 'test',
+      body: 'Hello, world！'
+    )
+  end
+
+  it 'ファクトリが有効であること' do
+    expect(@post).to be_valid
+  end
 end
