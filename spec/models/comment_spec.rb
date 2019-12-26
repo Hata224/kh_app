0140aaf336ef17e9575e9692e5ef16acb8b31bb8
Hata <kh6305su@gmail.com>
2019-12-26 18:24:31 +0900

    [update]バリデーションに関するテストを追記

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  describe 'コメントのバリデーションについての検証' do
+    before do
+      @user = FactoryBot.create(:user)
+      @comment = @user.comments.create(
+        content: 'Hello, world!'
+      )
+    end
+    it 'コメントが空欄であるなら無効であること' do
+      @comment.content = nil
+      @comment.valid?
+      expect(@comment.errors).to be_added(:content, :blank)
+    end
+  end
 end
