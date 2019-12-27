42b4f362271f2b393135dfc30f05845d82db1fa8
Hata <kh6305su@gmail.com>
2019-12-27 18:30:51 +0900

    [update]テストを追記

---
+++ spec/system/user_sign_in_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'ログインに関するテスト', type: :system do
+  before do
+    visit new_user_session_path
+  end
+
+  describe 'ログインが有効である場合' do
+    before do
+      user = FactoryBot.create(:user)
+      fill_in 'メールアドレス', with: user.email
+      fill_in 'パスワード', with: user.password
+      click_button 'ログイン'
+    end
+
+    it 'ユーザー詳細画面にリダイレクトされること' do
+      expect(current_path).to eq(pages_show_path)
+    end
+  end
+end
