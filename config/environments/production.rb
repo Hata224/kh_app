b64dd76d58dc7587fa1ce4be31552eb0c7e4875b
Hata <kh6305su@gmail.com>
2020-01-18 00:11:27 +0900

    [update]本番環境でもdeviseのメール認証機能を有効化

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "kh_app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'kh6305su@gmail.com' }
+  config.action_mailer.default_url_options = { host: 'clacopi.com' }
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -92,6 +93,18 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'kh6305su@gmail.com',
+    password: 'klhujxtpkiljclfw',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.serve_static_files = true
 end
