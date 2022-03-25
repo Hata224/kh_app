e574c1c3d46f59f7f20c9545ece29b07a4dd0072
Hata <kh6305su@gmail.com>
2022-03-25 12:38:17 +0900

    Refactoring comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_post, only: %i[create destroy validate_user]
   before_action :validate_user, only: %i[create destroy]
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      redirect_to post_path(@post), notice: '提案しました'
+       flash[:notice] = '提案しました'
     elsif @comment.content.empty?
-      redirect_to post_path(@post), alert: 'メッセージを入力してください'
+       flash[:alert] = 'メッセージを入力してください'
     else
-      redirect_to post_path(@post), alert: '500文字以内で入力してください'
+       flash[:alert] = '500文字以内で入力してください'
     end
+    redirect_to post_path(@post)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
-    @comment.destroy
     if @comment.destroy
-      redirect_to post_path(@post), notice: '投稿を削除しました'
+      flash[:notice] = '投稿を削除しました'
     else
-      redirect_to post_path(@post), alerts: '投稿を削除できませんでした'
+      flash[:alert] = '投稿を削除できませんでした'
     end
+    redirect_to post_path(@post)
   end
 
   private
@@ -33,8 +33,11 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:content)
   end
 
-  def validate_user
+  def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def validate_user
     @comment = Comment.find_by(post_id: params[:post_id])
     redirect_to post_path(@post), alert: '投稿者はこの操作はできません' if @post.user_id == current_user.id
   end
